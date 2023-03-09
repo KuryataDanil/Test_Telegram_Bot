a63fb649121da8675b05c85757b5e179ed6b87d0
DanilK17 <danil.17.4.2004@gmail.com>
2023-03-09 18:39:56 +0300

    bag fix

--- telegram_bot/handlers/client.py
+++ telegram_bot/handlers/client.py
@@ -2,21 +2,21 @@ from aiogram import types, Dispatcher
 from create_bot import dp, bot
 
 #Start
-#@dp.message_handler(commands = "start")
+@dp.message_handler(commands = "start")
 async def command_start(message : types.Message):
 	try:
-		await bot.send_message(message.from_user.id, "Здравсвуйте, чем я могу быть полезен?\nВот список моих команд")
+		await bot.send_message(message.from_user.id, 'Здравсвуйте, чем я могу быть полезен?\nВот список моих команд')
 	except:
-		 await message.reply("Напишите боту в ЛС, вот ссылка:\nhttp://t.me/My_Test61_Bot")
+		await message.reply("Напишите боту в ЛС, вот ссылка:\nhttp://t.me/My_Test61_Bot")
 
 
 #Help
-#@dp.message_handler(commands = "help")
+@dp.message_handler(commands = "help")
 async def command_help(message : types.Message):
 	try:
-		await bot.send_message(message.from_user.id, "Вот что я умею")
+		await bot.send_message(message.from_user.id, 'Вот что я умею')
 	except:
-		 await message.reply("Напишите боту в ЛС, вот ссылка:\nhttp://t.me/My_Test61_Bot")
+		await message.reply("Напишите боту в ЛС, вот ссылка:\nhttp://t.me/My_Test61_Bot")
 
 
 def register_handlers_client(dp : Dispatcher):
--- telegram_bot/handlers/other.py
+++ telegram_bot/handlers/other.py
@@ -1,5 +1,5 @@
 from aiogram import types, Dispatcher
-from create_bot import dp
+from create_bot import dp, bot
 import json, string
 
 
@@ -11,9 +11,10 @@ async def echo_send(message: types.Message):
 	if {i.lower().translate(str.maketrans('', '', string.punctuation)) for i in message.text.split(' ')}.intersection(set(json.load(open('cenz.json')))) != set():
 		await message.reply('Маты запрещены')
 		await message.delete()
+	'''
 	else:
 		await bot.send_message(message.from_user.id, message.text)
-	'''
+	
 	await message.answer(message.text)
 	await message.reply(message.text)
 	await bot.send_message(message.from_user.id,message)
